a994edfdcb38a42d9059e8fe74532220fb3b953b
muklisxtc24 <87222847+muklisxtc24@users.noreply.github.com>
2021-07-28 21:29:15 +0700

    Add files via upload

---
+++ Muklis.js
@@ -0,0 +1,43 @@
+function arrayToObject(arr){
+    var date = new Date()
+    var data1 = date.getFullYear() - arr["1. Abduh Muhammad"][3]
+    var data2 = date.getFullYear() - arr["2. Ahmad Taufik"][3]
+
+    var people1 = {
+        firstName : arr["1. Abduh Muhammad"][0],
+        lastName : arr["1. Abduh Muhammad"][1],
+        gender : arr["1. Abduh Muhammad"][2],
+        age : data1
+    }
+
+    var people2 = {
+        firstName : arr["2. Ahmad Taufik"][0],
+        lastName : arr["2. Ahmad Taufik"] [1],
+        gender : arr["2. Ahmad Taufik"] [2],
+        age : data2
+    }
+    
+    console.log('1. Abduh Muhammad : ',people1);
+    console.log('\n');
+    console.log('2. Ahmad Taufik : ',people2);
+    
+}
+
+var people = {
+    "1. Abduh Muhammad" : [
+        "Abduh",
+        "Muhammad",
+        "male",
+        1992,
+    ],
+    "2. Ahmad Taufik" : [
+        "Ahmad",
+        "Taufik",
+        "male",
+        1985
+    ]
+
+    
+}
+arrayToObject(people);
+        
